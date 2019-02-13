f50914374d3fc79b474e9db8b297eca2f89d0a5b
fukuchiyuki1225 <44431860+fukuchiyuki1225@users.noreply.github.com>
2019-02-14 02:08:11 +0900

    アイテムリストと棚の切り替えができるよう変更

--- slime_in_bottle/Assets/Scripts/GameManager.cs
+++ slime_in_bottle/Assets/Scripts/GameManager.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject flont_UI, back_UI;
+    Color flont_c, back_c;
+    int UI_flag = 0;
+    [SerializeField] Sprite[] items;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Change_UI()
+    {
+        flont_c = flont_UI.GetComponent<Image>().color;
+        back_c = back_UI.GetComponent<Image>().color;
+
+        flont_UI.GetComponent<Image>().color = back_c;
+        back_UI.GetComponent<Image>().color = flont_c;
+
+        UI_flag = 1 - UI_flag;
+
+        if(UI_flag == 0)
+        {
+
+        }
+        else
+        {
+            for(int i = 0; i <= 9; i++)
+            {
+
+            }
+        }
+    }
 }
